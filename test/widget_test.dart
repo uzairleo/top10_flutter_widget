1202e93b97a0eb84f649cb593930596e3615fb36
Uzair Leo <uzair.jan336@gmail.com>
2020-04-21 21:41:11 +0500

    add flutter launch object

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:top10_flutter_widgets/main.dart';
+import 'package:top10flutterwid/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
