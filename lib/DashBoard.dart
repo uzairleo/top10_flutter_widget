f795117b5d65bee9f5f91676d174bf80e07e9aba
Uzair Leo <uzair.jan336@gmail.com>
2020-04-22 02:28:46 +0500

    upgrade flutter and files

--- lib/DashBoard.dart
+++ lib/DashBoard.dart
@@ -11,156 +11,44 @@ class DashBoard extends StatefulWidget {
 class _DashBoardState extends State<DashBoard> {
   @override
   Widget build(BuildContext context) {
-    return 
-    BackdropScaffold(
-      frontLayer: Center(child:Text("I am front layer")),
-      backLayer: Center(child:Text("i am back layer")),
-      title: Text("testing"),
+    return BackdropScaffold(
+      // actions: <Widget>[
+      // IconButton(icon: Icon(Icons.ac_unit),onPressed: ()=>{},)
+      // ],
+      frontLayerBorderRadius: BorderRadius.only(
+          topLeft: Radius.circular(18.0), topRight: Radius.circular(18.0)),
+      headerHeight: 50.0,
+
       iconPosition: BackdropIconPosition.leading,
+      title: RichText(
+        text: TextSpan(
+            text: "TOP10",
+            style: TextStyle(
+              color: Colors.teal,
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+            ),
+            children: [
+              TextSpan(
+                text: "Flutter",
+                style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              TextSpan(
+                text: "Widgets",
+                style: TextStyle(
+                  color: Colors.purple,
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ]),
+      ),
+      frontLayer: Home(),
+      backLayer: BackHome(),
     );
-    // BackdropScaffold(
-    //   // actions: <Widget>[
-    //   // IconButton(icon: Icon(Icons.ac_unit),onPressed: ()=>{},)
-    //   // ],
-    //   frontLayerBorderRadius: BorderRadius.only(
-    //       topLeft: Radius.circular(18.0), topRight: Radius.circular(18.0)),
-    //   headerHeight: 50.0,
-
-    //   iconPosition: BackdropIconPosition.leading,
-    //   title: RichText(
-    //     text: TextSpan(
-    //         text: "TOP10",
-    //         style: TextStyle(
-    //           color: Colors.teal,
-    //           fontSize: 24.0,
-    //           fontWeight: FontWeight.bold,
-    //         ),
-    //         children: [
-    //           TextSpan(
-    //             text: "Flutter",
-    //             style: TextStyle(
-    //               color: Colors.blueAccent,
-    //               fontSize: 24.0,
-    //               fontWeight: FontWeight.bold,
-    //             ),
-    //           ),
-    //           TextSpan(
-    //             text: "Widgets",
-    //             style: TextStyle(
-    //               color: Colors.purple,
-    //               fontSize: 22.0,
-    //               fontWeight: FontWeight.bold,
-    //             ),
-    //           )
-    //         ]),
-    //   ),
-
-    //   // backgroundColor: Colors.grey,
-    //   // appBar: AppBar(
-
-    //   //   elevation: 20.0,
-    //   //   backgroundColor: Colors.orangeAccent,
-    //   //  centerTitle: true,
-    //   //   title: RichText(
-    //   //   text: TextSpan(
-    //   //     text: "TOP10",
-    //   //     style: TextStyle(
-    //   //       color: Colors.teal,
-    //   //       fontSize: 24.0,
-    //   //     fontWeight: FontWeight.bold,
-    //   //     ),
-    //   //     children: [
-    //   //       TextSpan(
-    //   //         text: "Flutter",
-    //   //     style: TextStyle(
-    //   //       color: Colors.blueAccent,
-    //   //       fontSize: 24.0,
-    //   //       fontWeight: FontWeight.bold,
-    //   //     ),
-    //   //       ),
-    //   //       TextSpan(
-    //   //         text: "Widgets",
-    //   //       style: TextStyle(
-    //   //       color: Colors.purple,
-    //   //       fontSize: 24.0,
-    //   //       fontWeight: FontWeight.bold,
-    //   //     ),
-    //   //       )
-    //   //     ]
-    //   //   ),
-    //   //   ),
-    //   // ),
-
-    //   frontLayer: Home(),
-    //   backLayer: BackHome(),
-    //   // backLayer:  Center(
-    //   //     child: Switch(
-    //   //       value: darkThemeEnabled,
-    //   //       onChanged:(changedTheme)
-    //   //       {
-    //   //         setState(() {
-    //   //           darkThemeEnabled=changedTheme;
-    //   //         });
-    //   //       },
-    //   //     ),
-    //   //   )
-    //   // backLayer: BackHome(bcolor,appbarcolor),
-    //   // backLayer: Center(
-    //   //   child:
-    //   //   Column(
-    //   // mainAxisAlignment:MainAxisAlignment.start,
-    //   // children :<Widget>[
-    //   //   Text("data"),
-    //   //   Text("data"),
-    //   //   Text("data"),
-    //   //   Text("data")
-    //   //  Row(
-    //   //    children:<Widget>[
-    //   //     //  Image.asset('/images/circle-cropped.png'),
-    //   //      Text( "_discription"),
-    //   //      ListTile(
-    //   //       leading: Icon(Icons.code),
-    //   //       title: Text("Code"),
-    //   //       subtitle: Text("Source Code on Github"),
-    //   //      onTap: (){},
-    //   //      ),
-    //   //      Divider(),
-    //   //      ListTile(
-    //   //        title: Text("Code"),
-    //   //       leading: Icon(Icons.code),
-    //   //       subtitle: Text("Source Code offline"),
-    //   //         onTap: (){},
-    //   //          ),
-
-    //   //         //    ])
-    //   //   ]
-
-    //   // ),
-    //   // ),
-
-    //   // floatingActionButtonLocation: FloatingActionButtonLocation.,
-    //   // floatingActionButton: FloatingActionButton(
-    //   //   backgroundColor: Colors.deepOrange,
-    //   //   child: Icon(Icons.info),
-    //   //   onPressed: (){
-
-    //   //  showDialog(
-    //   //     context: context,
-    //   //     builder: (BuildContext context){
-    //   //       return CupertinoAlertDialog(
-
-    //   //     title: Text("Contact us",style:Theme.of(context).textTheme.title),
-    //   //     content: Text("I am uzair leo from islamia college university of peshwar a software developer"),
-    //   //     actions: <Widget>[
-    //   //       FlatButton(
-    //   //         child: Text("OK"),
-    //   //         onPressed: ()=>Navigator.pop(context),
-    //   //       )
-    //   //     ],
-    //   //   );
-
-    //   //     }
-    // );
-    
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,208 +1,167 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-// import 'package:top10flutterwid/DashBoard.dart';
+import 'package:top10flutterwid/DashBoard.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-// import 'package:top10flutterwid/BackHome.dart';
-// import 'package:top10flutterwid/Home.dart';
-import 'package:backdrop/backdrop.dart';
-// // import 'package:flutter/foundation.dart'
 
 void main() {
   runApp(new MyApp());
 }
+bool darkThemeEnabled = false;
+
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  Color appBarTheme = Colors.orangeAccent;
+  Brightness myBrightness = Brightness.dark;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:BackdropScaffold(
-        title: Text("Backdrop Navigation Example"),
-        iconPosition: BackdropIconPosition.leading,
-        actions: <Widget>[
-          BackdropToggleButton(
-            icon: AnimatedIcons.list_view,
-          ),
-        ],
-        frontLayer:Center(child:Text("uzairleoFront Layer")),
-        backLayer: BackdropNavigationBackLayer(
-          items: [
-            ListTile(title: Text("Widget 1")),
-            ListTile(title: Text("Widget 2")),
+      debugShowCheckedModeBanner: false,
+      title: "Top10Widgets",
+      home:
+          // Text("leo")
+          AppBody(),
+      theme: ThemeData(
+          primaryColor: appBarTheme,
+          brightness: darkThemeEnabled ? Brightness.dark : Brightness.light,
+          // accentColor: Colors.orangeAccent,
+          appBarTheme: AppBarTheme(color: appBarTheme)),
+    );
+  }
+}
+
+class AppBody extends StatefulWidget {
+  // AppBody();
+  @override
+  _AppBodyState createState() => _AppBodyState();
+}
+
+class _AppBodyState extends State<AppBody> {
+  double radius1 = 70;
+  double radius2 = 50;
+  double radius3 = 35;
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 5), () {
+      setState(() {
+        radius3 = 65;
+        radius2 = 60;
+      });
+       Navigator.push(
+       context,
+       MaterialPageRoute(
+         builder: (context)=>(DashBoard())
+       ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.orangeAccent,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 100.0,
+            ),
+            AnimatedContainer(
+              duration: Duration(seconds: 3),
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                     Navigator.push(context, MaterialPageRoute(
+                      builder: (context)=> (DashBoard())
+                     ));
+                  });
+                },
+                child: CircleAvatar(
+                  radius: radius1,
+                  backgroundColor: Colors.purple,
+                  child: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: radius2,
+                    child: FlutterLogo(
+                      size: radius3,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            AnimatedContainer(
+              duration: Duration(seconds: 2),
+              alignment: Alignment.center,
+              child: RichText(
+                text: TextSpan(
+                    text: "TOP10",
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: "Flutter",
+                        style: TextStyle(
+                          color: Colors.blueAccent,
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(
+                        text: "Widgets",
+                        style: TextStyle(
+                          color: Colors.purple,
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ]),
+              ),
+            ),
+            SizedBox(
+              height: 110.0,
+            ),
+            SpinKitChasingDots(
+              color: Colors.blue,
+            ),
+            SizedBox(
+              height: 18.0,
+            ),
+            RichText(
+              text: TextSpan(
+                  text: " Loading...",
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 14.0,
+                  ),
+                  children: null),
+            ),
+            SizedBox(
+              height: 60.0,
+            ),
+            RichText(
+              text: TextSpan(
+                  text: " Version 1.0\n   UzairLeo",
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 14.0,
+                  ),
+                  children: null),
+            ),
           ],
-          // onTap: ,
         ),
       ),
     );
   }
 }
-// // show debugDefaultTargetPlatformOverride; // for desktop embedder
-
-// void main()
-// {
-//   // debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia; //
-//   runApp(new MyApp());
-
-// // }
-// bool darkThemeEnabled = false;
-
-// class MyApp extends StatefulWidget {
-//   @override
-//   _MyAppState createState() => _MyAppState();
-// }
-
-// class _MyAppState extends State<MyApp> {
-//   Color appBarTheme = Colors.orangeAccent;
-//   Brightness myBrightness = Brightness.dark;
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: "Top10Widgets",
-//       home:
-//           // Text("leo")
-//           AppBody(),
-//       theme: ThemeData(
-//           primaryColor: appBarTheme,
-//           brightness: darkThemeEnabled ? Brightness.dark : Brightness.light,
-//           // accentColor: Colors.orangeAccent,
-//           appBarTheme: AppBarTheme(color: appBarTheme)),
-//     );
-//   }
-// }
-
-// class AppBody extends StatefulWidget {
-//   // AppBody();
-//   @override
-//   _AppBodyState createState() => _AppBodyState();
-// }
-
-// class _AppBodyState extends State<AppBody> {
-//   double radius1 = 70;
-//   double radius2 = 50;
-//   double radius3 = 35;
-//   @override
-//   void initState() {
-//     super.initState();
-
-//     Future.delayed(Duration(seconds: 4), () {
-//       setState(() {
-//         radius3 = 65;
-//         radius2 = 60;
-//       });
-//       //  Navigator.push(
-//       //  context,
-//       //  MaterialPageRoute(
-//       //    builder: (context)=>(DashBoard())
-//       //  ));
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       color: Colors.orangeAccent,
-//       child: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.start,
-//           crossAxisAlignment: CrossAxisAlignment.center,
-//           children: <Widget>[
-//             SizedBox(
-//               height: 100.0,
-//             ),
-//             AnimatedContainer(
-//               duration: Duration(seconds: 3),
-//               child: GestureDetector(
-//                 onTap: () {
-//                   setState(() {
-//                     //  Navigator.push(context, MaterialPageRoute(
-//                     //   builder: (context)=> (DashBoard())
-//                     //  ));
-//                   });
-//                 },
-//                 child: CircleAvatar(
-//                   radius: radius1,
-//                   backgroundColor: Colors.purple,
-//                   child: CircleAvatar(
-//                     backgroundColor: Colors.white,
-//                     radius: radius2,
-//                     child: FlutterLogo(
-//                       size: radius3,
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//             ),
-//             SizedBox(
-//               height: 30.0,
-//             ),
-//             AnimatedContainer(
-//               duration: Duration(seconds: 2),
-//               alignment: Alignment.center,
-//               child: RichText(
-//                 text: TextSpan(
-//                     text: "TOP10",
-//                     style: TextStyle(
-//                       color: Colors.teal,
-//                       fontSize: 24.0,
-//                       fontWeight: FontWeight.bold,
-//                     ),
-//                     children: [
-//                       TextSpan(
-//                         text: "Flutter",
-//                         style: TextStyle(
-//                           color: Colors.blueAccent,
-//                           fontSize: 24.0,
-//                           fontWeight: FontWeight.bold,
-//                         ),
-//                       ),
-//                       TextSpan(
-//                         text: "Widgets",
-//                         style: TextStyle(
-//                           color: Colors.purple,
-//                           fontSize: 22.0,
-//                           fontWeight: FontWeight.bold,
-//                         ),
-//                       ),
-//                     ]),
-//               ),
-//             ),
-//             SizedBox(
-//               height: 110.0,
-//             ),
-//             SpinKitChasingDots(
-//               color: Colors.blue,
-//             ),
-//             SizedBox(
-//               height: 18.0,
-//             ),
-//             RichText(
-//               text: TextSpan(
-//                   text: " Loading...",
-//                   style: TextStyle(
-//                     color: Colors.black54,
-//                     fontSize: 14.0,
-//                   ),
-//                   children: null),
-//             ),
-//             SizedBox(
-//               height: 60.0,
-//             ),
-//             RichText(
-//               text: TextSpan(
-//                   text: " Version 1.0\n   UzairLeo",
-//                   style: TextStyle(
-//                     color: Colors.black54,
-//                     fontSize: 14.0,
-//                   ),
-//                   children: null),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
